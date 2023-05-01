bbc4ce4be355aca04728606737a9401d0b7b6eaf
tamaneko <martinlee7829@gmail.com>
2023-05-01 21:52:32 +0800

    十問まで

---
+++ Summing Digits.py
@@ -0,0 +1,21 @@
+from functools import reduce
+
+
+def f(n):
+    if (len(n) == 1):
+        print(int(n))
+    else:
+        l = map(lambda x: int(x), n)
+        r = reduce(lambda x, y: x+y, list(l))
+        return f(str(r))
+
+
+while True:
+    try:
+        s = input()
+        if (s == "0"):
+            break
+        else:
+            f(s)
+    except ValueError:
+        break
